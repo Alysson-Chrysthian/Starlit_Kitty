9b85de63bbf2dfd1e587226d03729b58b6552ad7
Alysson-Chrysthian <alyssonchrysthian@gmail.com>
2025-10-20 19:32:35 -0300

    intro duration incresed

--- scripts/scenes/intro.gd
+++ scripts/scenes/intro.gd
@@ -1,6 +1,7 @@
 extends ColorRect
 
 func _on_fade_in_animation_finished(anim_name: StringName) -> void:
+	await get_tree().create_timer(0.5).timeout
 	$fade_out.play("fade_out")
 	
 func _on_fade_out_animation_finished(anim_name: StringName) -> void:
